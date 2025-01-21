77652365c4f42ec313ca268fc41bd7a37580fdc1
Anurag Kumbhare <anurag@MacBook-Air.local>
2025-01-21 12:53:02 +0530

    logo changed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -10,12 +10,11 @@ const Navbar = () => {
     <div className="w-full bg-white fixed top-0 left-0 right-0 z-50 shadow-md">
       <div className="mx-auto flex max-w-7xl items-center justify-between px-4 py-2 sm:px-6 lg:px-8">
         {/* Logo Section */}
-        <div className="inline-flex items-center space-x-2">
+        <div className="inline-flex items-center space-x-2 pt-3.5 pb-3.5">
           <Link to="/">
             <img src={logo} alt="Logo" className="w-44 max-md:w-28 cursor-pointer" />
           </Link>
         </div>
-
         {/* Links */}
         <div className="hidden lg:flex space-x-8">
           <Link to="/product-page" className="text-sm text-gray-800 hover:text-gray-900">
